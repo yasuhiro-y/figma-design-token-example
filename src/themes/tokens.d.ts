05abb5b7b9f4071c7a4acbb5ab25102ae5757641
Yasuhiro Yokota <yy@terass.com>
2022-08-17 02:59:16 +0900

    setup style dictionary files

---
+++ src/themes/tokens.d.ts
@@ -0,0 +1,94 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 16 Aug 2022 17:56:26 GMT
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value: any;
+  name?: string;
+  comment?: string;
+  themeable?: boolean;
+  attributes?: {
+    category?: string;
+    type?: string;
+    item?: string;
+    subitem?: string;
+    state?: string;
+    [key: string]: any;
+  };
+  [key: string]: any;
+}
+
+declare const tokens: {
+  "tokenSetOrder": {
+    "0": DesignToken,
+    "1": DesignToken
+  },
+  "text": {
+    "black": DesignToken,
+    "white": DesignToken,
+    "danger": DesignToken
+  },
+  "borderRadius": {
+    "sm": DesignToken,
+    "md": DesignToken,
+    "lg": DesignToken
+  },
+  "button": {
+    "primary": DesignToken,
+    "secondary": DesignToken,
+    "sm": DesignToken,
+    "md": DesignToken
+  },
+  "spacing": {
+    "2": DesignToken,
+    "3": DesignToken
+  },
+  "black": DesignToken,
+  "white": DesignToken,
+  "red": {
+    "50": DesignToken,
+    "100": DesignToken,
+    "200": DesignToken,
+    "300": DesignToken,
+    "400": DesignToken,
+    "500": DesignToken,
+    "600": DesignToken,
+    "700": DesignToken,
+    "800": DesignToken,
+    "900": DesignToken,
+    "950": DesignToken
+  },
+  "blue": {
+    "50": DesignToken,
+    "100": DesignToken,
+    "200": DesignToken,
+    "300": DesignToken,
+    "400": DesignToken,
+    "500": DesignToken,
+    "600": DesignToken,
+    "700": DesignToken,
+    "800": DesignToken,
+    "900": DesignToken,
+    "950": DesignToken
+  },
+  "grey": {
+    "50": DesignToken,
+    "100": DesignToken,
+    "200": DesignToken,
+    "300": DesignToken,
+    "400": DesignToken,
+    "500": DesignToken,
+    "600": DesignToken,
+    "700": DesignToken,
+    "800": DesignToken,
+    "900": DesignToken,
+    "950": DesignToken
+  },
+  "primitiveSize": {
+    "1": DesignToken,
+    "2": DesignToken
+  }
+}
